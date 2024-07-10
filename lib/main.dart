89158c834edb9faeafb1c169ec21ccc3d1ac0299
yuko87 <htms.fe.cmsm@gmail.com>
2024-07-10 17:28:06 +0900

    ）削除

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
