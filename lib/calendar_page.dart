7e86fb0a29067fdbeec1e32263f9aeb909d7d1a8
haseyuy <haseyuy@gmail.com>
2024-07-12 14:40:18 +0900

    feat: Add calendar page with table calendar widget

---
+++ lib/calendar_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({super.key});
+
+  @override
+  CalendarPageState createState() => CalendarPageState();
+}
+
+class CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  final DateTime _now = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TableCalendar(
+        firstDay: _now,
+        lastDay: DateTime.utc(2030, 12, 31),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay; // update `_focusedDay` here as well
+          });
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+        },
+      ),
+    );
+  }
+}
